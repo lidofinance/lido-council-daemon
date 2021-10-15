9e37b3de1c1309aca1223811a60bd5b9e5a52747
George Avsetsin <mail@sjors.ru>
2021-10-15 11:50:42 +0300

    feat: new goerli deploy

--- src/security/security.constants.ts
+++ src/security/security.constants.ts
@@ -4,7 +4,7 @@ export const DEPOSIT_SECURITY_BY_NETWORK: {
   [key in CHAINS]?: string;
 } = {
   [CHAINS.Mainnet]: '0x0000000000000000000000000000000000000000', // TODO
-  [CHAINS.Goerli]: '0x071954e987ee02BCAF4b29D6bD533A32E11607f3',
+  [CHAINS.Goerli]: '0x3E4Fa99107aD166380A9F5523BbacAcD1e24909C',
 };
 
 export const getDepositSecurityAddress = (chainId: CHAINS): string => {
