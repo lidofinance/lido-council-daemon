51de77452a82ffcbeb8de58fabeb97dae120f940
George Avsetsin <mail@sjors.ru>
2021-10-15 16:54:27 +0300

    feat: allow run without private key

--- src/wallet/wallet.constants.ts
+++ src/wallet/wallet.constants.ts
@@ -1,1 +1,4 @@
+import { WeiPerEther } from '@ethersproject/constants';
+
 export const WALLET_PRIVATE_KEY = 'walletPrivateKey';
+export const WALLET_MIN_BALANCE = WeiPerEther.div(2);
--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { Configuration } from 'common/config';
+import { ProviderModule } from 'provider';
 import { WALLET_PRIVATE_KEY } from './wallet.constants';
 import { WalletService } from './wallet.service';
 
 @Module({
+  imports: [ProviderModule],
   providers: [
     WalletService,
     {
