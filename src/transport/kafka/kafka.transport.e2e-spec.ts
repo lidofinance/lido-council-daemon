977368e065a194446b9881a74ef9a3b55c0b08a5
George Avsetsin <mail@sjors.ru>
2022-11-02 19:13:52 +0400

    fix: e2e tests

--- src/transport/kafka/kafka.transport.e2e-spec.ts
+++ src/transport/kafka/kafka.transport.e2e-spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { LoggerService } from '@nestjs/common';
 import { LoggerModule } from 'common/logger';
 import { ConfigModule } from 'common/config';
+import { MockProviderModule } from 'provider';
 import { KafkaTransport } from './kafka.transport';
 import { Kafka } from 'kafkajs';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { MessageType } from '../../messages';
 
 describe('KafkaTransport', () => {
   let transport: KafkaTransport;
   let moduleRef: TestingModule;
+  let loggerService: LoggerService;
 
   beforeEach(async () => {
     moduleRef = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot(), LoggerModule],
+      imports: [
+        ConfigModule.forRoot(),
+        MockProviderModule.forRoot(),
+        LoggerModule,
+      ],
       providers: [
         KafkaTransport,
         {
@@ -27,6 +35,10 @@ describe('KafkaTransport', () => {
     }).compile();
 
     transport = moduleRef.get(KafkaTransport);
+    loggerService = moduleRef.get(WINSTON_MODULE_NEST_PROVIDER);
+
+    jest.spyOn(loggerService, 'log').mockImplementation(() => undefined);
+    jest.spyOn(loggerService, 'debug').mockImplementation(() => undefined);
   });
 
   afterEach(async () => {
@@ -38,12 +50,12 @@ describe('KafkaTransport', () => {
     it('should send two messages to topic and read two messages from topic', async () => {
       const receivedMessages: any[] = [];
 
-      await transport.publish('test', { label: 'first' }, MessageType.PING);
-      await transport.publish('test', { label: 'second' }, MessageType.PING);
-
       await transport.subscribe('test', MessageType.PING, async (msg) => {
         receivedMessages.push(msg);
       });
+
+      await transport.publish('test', { label: 'first' }, MessageType.PING);
+      await transport.publish('test', { label: 'second' }, MessageType.PING);
 
       await new Promise<void>(async (resolve) => {
         setTimeout(resolve, 3000);
@@ -54,6 +66,6 @@ describe('KafkaTransport', () => {
       expect(receivedMessages[0].label).toBe('first');
       expect(receivedMessages[1]).toHaveProperty('label');
       expect(receivedMessages[1].label).toBe('second');
-    });
+    }, 10_000);
   });
 });
