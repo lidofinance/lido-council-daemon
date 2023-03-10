faa7d1119cd44cf7b879ddfbc527dff4ca1f97f9
infloop <infloop.gm@gmail.com>
2023-03-10 18:25:07 +0300

    feat: add support for Zhejiang V2 (re-deploy)

--- src/contracts/repository/repository.constants.ts
+++ src/contracts/repository/repository.constants.ts
@@ -5,7 +5,7 @@ export const LIDO_BY_NETWORK: {
 } = {
   [CHAINS.Mainnet]: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
   [CHAINS.Goerli]: '0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F',
-  [CHAINS.Zhejiang]: '0xEC9ac956D7C7fE5a94919fD23BAc4a42f950A403',
+  [CHAINS.Zhejiang]: '0xDe82ADEd58dA35add75Ea4676239Ca169c8dCD15',
 };
 
 export const getLidoAddress = (chainId: CHAINS): string => {
@@ -20,7 +20,7 @@ export const DEPOSIT_SECURITY_BY_NETWORK: {
 } = {
   [CHAINS.Mainnet]: '0x710B3303fB508a84F10793c1106e32bE873C24cd',
   [CHAINS.Goerli]: '0x7DC1C1ff64078f73C98338e2f17D1996ffBb2eDe',
-  [CHAINS.Zhejiang]: '0x48bEdD13FF63F7Cd4d349233B6a57Bff285f8E32',
+  [CHAINS.Zhejiang]: '0xaaB7034eB0C0556c61c4E2F5B9884abf9EE357c9',
 };
 
 export const getDepositSecurityAddress = (chainId: CHAINS): string => {
