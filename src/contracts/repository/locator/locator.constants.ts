67ee7b2eb753b5b76862ea8b171c09bcf1f7f1ff
Eddort <webisforce@gmail.com>
2023-04-10 13:59:12 +0400

    fix: addr

--- src/contracts/repository/locator/locator.constants.ts
+++ src/contracts/repository/locator/locator.constants.ts
@@ -4,8 +4,8 @@ export const LIDO_LOCATOR_BY_NETWORK: {
   [key in CHAINS]?: string;
 } = {
   [CHAINS.Mainnet]: '0x0000000000000000000000000000000000000000',
-  [CHAINS.Goerli]: '0xFd5B65B7A17Fd5ebE882b9907793071235c5E943',
-  [CHAINS.Zhejiang]: '0xbeB7bdCB1948A065789f2b72409f39dCABFcCe52',
+  [CHAINS.Goerli]: '0x1eDf09b5023DC86737b59dE68a8130De878984f5',
+  [CHAINS.Zhejiang]: '0x548C1ED5C83Bdf19e567F4cd7Dd9AC4097088589',
 };
 
 export const getLidoLocatorAddress = (chainId: CHAINS): string => {
