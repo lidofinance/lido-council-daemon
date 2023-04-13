24d072d52d88855526c9c00e2d0e1260c93e3159
Eddort <webisforce@gmail.com>
2023-04-13 19:45:49 +0400

    feat: add mainnet locator address

--- src/contracts/repository/locator/locator.constants.ts
+++ src/contracts/repository/locator/locator.constants.ts
@@ -3,7 +3,7 @@ import { CHAINS } from '@lido-sdk/constants';
 export const LIDO_LOCATOR_BY_NETWORK: {
   [key in CHAINS]?: string;
 } = {
-  [CHAINS.Mainnet]: '0x0000000000000000000000000000000000000000',
+  [CHAINS.Mainnet]: '0xC1d0b3DE6792Bf6b4b37EccdcC24e45978Cfd2Eb',
   [CHAINS.Goerli]: '0x1eDf09b5023DC86737b59dE68a8130De878984f5',
   [CHAINS.Zhejiang]: '0x548C1ED5C83Bdf19e567F4cd7Dd9AC4097088589',
 };
