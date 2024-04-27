cc860360b15c2807aea70ef8b8baff636d911e0b
Eddort <webisforce@gmail.com>
2024-04-27 15:05:46 +0200

    wip: remove old cache module

--- src/cache/cache.constants.ts
+++
@@ -1,5 +0,0 @@
-export const CACHE_DIR = 'cache';
-
-export const CACHE_FILE_NAME = 'cacheFileName';
-export const CACHE_DEFAULT_VALUE = 'cacheDefaultValue';
-export const CACHE_BATCH_SIZE = 'cacheBatchSize';
