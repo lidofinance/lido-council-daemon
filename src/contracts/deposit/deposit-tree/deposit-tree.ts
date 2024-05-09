e61548e86b16b265e18c771f3e355c92137676ca
Eddort <webisforce@gmail.com>
2024-05-09 09:16:02 +0200

    refactor: deposit-tree

--- src/contracts/deposit/deposit-tree/deposit-tree.ts
+++ src/contracts/deposit/deposit-tree/deposit-tree.ts
@@ -1,15 +1,10 @@
 import { ethers } from 'ethers';
 import { digest2Bytes32 } from '@chainsafe/as-sha256';
 import { fromHexString } from '@chainsafe/ssz';
-import { parseLittleEndian64 } from './deposit.utils';
+import { parseLittleEndian64, toLittleEndian64 } from '../deposit.utils';
 import { DepositData } from 'bls/bls.containers';
+import { NodeData } from '../interfaces';
 
-type NodeData = {
-  pubkey: string;
-  wc: string;
-  amount: string;
-  signature: string;
-};
 export class DepositTree {
   static DEPOSIT_CONTRACT_TREE_DEPTH = 32;
   static ZERO_HASH = fromHexString(
@@ -23,6 +18,9 @@ export class DepositTree {
     this.formZeroHashes();
   }
 
+  /**
+   * Initializes the zero hashes used in the tree.
+   */
   private formZeroHashes() {
     this.zeroHashes[0] = DepositTree.ZERO_HASH;
     for (
@@ -37,7 +35,16 @@ export class DepositTree {
     }
   }
 
-  private formBranch(node: Uint8Array, depositCount: number) {
+  /**
+   * Forms the branch of the tree needed to update the root when a new node is inserted.
+   * @param {Uint8Array} node - The node's data to be inserted.
+   * @param {number} depositCount - The sequential index of the deposit, representing the total deposits.
+   * @returns {Uint8Array[] | undefined} The updated branch of the tree after inserting the node.
+   */
+  private formBranch(
+    node: Uint8Array,
+    depositCount: number,
+  ): Uint8Array[] | undefined {
     let size = depositCount;
     for (
       let height = 0;
@@ -55,17 +62,29 @@ export class DepositTree {
     }
   }
 
+  /**
+   * Inserts a new deposit into the tree using detailed node data.
+   * @param {NodeData} nodeData - The detailed data of the deposit to be inserted.
+   */
   public insert(nodeData: NodeData) {
     const node = DepositTree.formDepositNode(nodeData);
     this.nodeCount++;
     this.formBranch(node, this.nodeCount);
   }
 
+  /**
+   * Inserts a new node into the tree using already computed node hash.
+   * @param {Uint8Array} node - The node's hash to be inserted.
+   */
   public insertNode(node: Uint8Array) {
     this.nodeCount++;
     this.formBranch(node, this.nodeCount);
   }
 
+  /**
+   * Computes and returns the root hash of the deposit tree.
+   * @returns {string} The computed root hash of the tree.
+   */
   public getRoot() {
     let node = DepositTree.ZERO_HASH;
     let size = this.nodeCount;
@@ -85,13 +104,17 @@ export class DepositTree {
       ['bytes', 'bytes', 'bytes'],
       [
         node,
-        this.toLittleEndian64(this.nodeCount),
+        toLittleEndian64(this.nodeCount),
         '0x000000000000000000000000000000000000000000000000',
       ],
     );
     return finalRoot;
   }
 
+  /**
+   * Creates a clone of the current tree instance, copying the branch structure and node count.
+   * @returns {DepositTree} A new DepositTree instance with the same state.
+   */
   public clone() {
     const tree = new DepositTree();
     tree.branch = [...this.branch];
@@ -99,6 +122,11 @@ export class DepositTree {
     return tree;
   }
 
+  /**
+   * Forms the deposit node from the given NodeData structure.
+   * @param {NodeData} nodeData - Detailed data of the deposit, including public key, withdrawal credentials, signature, and amount.
+   * @returns {Uint8Array} The hashed node as a Uint8Array.
+   */
   static formDepositNode(nodeData: NodeData): Uint8Array {
     return DepositData.hashTreeRoot({
       withdrawalCredentials: fromHexString(nodeData.wc),
@@ -106,11 +134,5 @@ export class DepositTree {
       signature: fromHexString(nodeData.signature),
       amount: parseLittleEndian64(nodeData.amount),
     });
-  }
-
-  private toLittleEndian64(value: number): string {
-    const buffer = Buffer.allocUnsafe(8);
-    buffer.writeBigUInt64LE(BigInt(value));
-    return '0x' + buffer.toString('hex');
   }
 }
