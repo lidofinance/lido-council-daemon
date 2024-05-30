aefcff8699956a6b9a56976b6ce121d28e0d650b
Anna Mukharram <amuhar3@gmail.com>
2024-05-30 20:10:49 +0400

    fix: lint

--- src/guardian/keys-validation/keys-validation.service.ts
+++ src/guardian/keys-validation/keys-validation.service.ts
@@ -1,5 +1,4 @@
-import { Inject, Injectable, LoggerService } from '@nestjs/common';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Injectable } from '@nestjs/common';
 import {
   KeyValidatorInterface,
   bufferFromHexString,
