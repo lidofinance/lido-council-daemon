d3766d4cf4d14d40073aedfe8f15996aae0bb923
Eddort <webisforce@gmail.com>
2024-09-16 09:21:47 +0200

    feat: remove old service

--- src/contracts/signing-key-events-cache/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './signing-key-events-cache.module';
-export * from './signing-key-events-cache.service';
--- src/contracts/signing-key-events-cache/leveldb/leveldb.constants.ts
+++
@@ -1,3 +0,0 @@
-export const DB_DIR = 'cache';
-export const DB_DEFAULT_VALUE = 'cacheDefaultValue';
-export const DB_LAYER_DIR = 'cache:layer';
