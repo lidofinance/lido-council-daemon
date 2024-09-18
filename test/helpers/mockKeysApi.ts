505d905213e9552ead52eeb02e8eac0e85810a1a
Anna Mukharram <amuhar3@gmail.com>
2024-09-18 16:41:13 +0400

    fix: removed comments

--- test/helpers/mockKeysApi.ts
+++ test/helpers/mockKeysApi.ts
@@ -2,49 +2,9 @@ import ethers from 'ethers';
 
 import { KeysApiService } from '../../src/keys-api/keys-api.service';
 import { SIMPLE_DVT, NOP_REGISTRY } from './../constants';
-// import { RegistryOperator } from 'keys-api/interfaces/RegistryOperator';
 import { SRModule } from 'keys-api/interfaces';
 import { ELBlockSnapshot } from 'keys-api/interfaces/ELBlockSnapshot';
 import { RegistryKey } from 'keys-api/interfaces/RegistryKey';
-
-// export const setupMockModules = (
-//   currentBlock: ethers.providers.Block,
-//   keysApiService: KeysApiService,
-//   mockedOperators: RegistryOperator[],
-//   mockedDvtOperators: RegistryOperator[],
-//   unusedKeys: RegistryKey[],
-// ) => {
-//   const curatedModule = mockedModule(currentBlock, currentBlock.hash);
-//   const sdvtModule = mockedModuleDvt(currentBlock, currentBlock.hash);
-//   const meta = mockedMeta(currentBlock, currentBlock.hash);
-
-//   mockedKeysApiOperatorsMany(
-//     keysApiService,
-//     [
-//       { operators: mockedOperators, module: curatedModule },
-//       { operators: mockedDvtOperators, module: sdvtModule },
-//     ],
-//     meta,
-//   );
-
-//   mockedKeysApiGetAllKeys(keysApiService, unusedKeys, meta);
-
-//   return { curatedModule, sdvtModule, meta };
-// };
-
-// export const setupKeysAPI = (
-//   block: ethers.providers.Block,
-//   keysApiService: KeysApiService,
-//   modules: SRModule[],
-//   keys: RegistryKey[],
-//   lastChangedBlockHash = undefined,
-// ) => {
-//   const meta = mockMeta(block, lastChangedBlockHash || block.hash);
-//   keysApiMockGetAllKeys(keysApiService, keys, meta);
-//   mod
-
-//   return { curatedModule, sdvtModule, meta };
-// };
 
 export const mockedModuleCurated: SRModule = {
   type: 'curated-onchain-v1',
@@ -89,62 +49,6 @@ export const mockMeta = (
   timestamp: block.timestamp,
   lastChangedBlockHash,
 });
-
-// export const mockOperator1 = {
-//   name: 'Dev team',
-//   rewardAddress: '0x6D725DAe055287f913661ee0b79dE6B21F12A459',
-//   stakingLimit: 12,
-//   stoppedValidators: 0,
-//   totalSigningKeys: 12,
-//   usedSigningKeys: 0,
-//   index: 0,
-//   active: true,
-//   moduleAddress: NOP_REGISTRY,
-// };
-
-// export const mockOperator2 = {
-//   name: 'Dev team',
-//   rewardAddress: '0x6D725DAe055287f913661ee0b79dE6B21F12A459',
-//   stakingLimit: 12,
-//   stoppedValidators: 0,
-//   totalSigningKeys: 12,
-//   usedSigningKeys: 0,
-//   index: 1,
-//   active: true,
-//   moduleAddress: NOP_REGISTRY,
-// };
-
-// export const mockedOperators: RegistryOperator[] = [
-//   mockOperator1,
-//   mockOperator2,
-// ];
-
-// export const mockedDvtOperator: RegistryOperator = {
-//   name: 'Dev DVT team',
-//   rewardAddress: '0x6D725DAe055287f913661ee0b79dE6B21F12A459',
-//   stakingLimit: 12,
-//   stoppedValidators: 0,
-//   totalSigningKeys: 12,
-//   usedSigningKeys: 0,
-//   index: 0,
-//   active: true,
-//   moduleAddress: SIMPLE_DVT,
-// };
-
-// export const mockedKeysApiOperatorsMany = (
-//   keysApiService: KeysApiService,
-//   data: { operators: RegistryOperator[]; module: SRModule }[],
-//   mockedMeta: ELBlockSnapshot,
-// ) => {
-//   jest
-//     .spyOn(keysApiService, 'getOperatorListWithModule')
-//     .mockImplementation(async () => ({
-//       data: data,
-//       meta: {
-//         elBlockSnapshot: mockedMeta,
-//       },
-//     }));
-// };
 
 export const keysApiMockGetModules = (
   keysApiService: KeysApiService,
