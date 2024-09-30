29508380eef1df87d21deff8ba9b2cd6fc5e3111
Eddort <webisforce@gmail.com>
2024-09-30 17:09:04 +0200

    refactor: mutex tests

--- src/contracts/data-bus/utils/mutex.spec.ts
+++ src/contracts/data-bus/utils/mutex.spec.ts
@@ -8,28 +8,21 @@ describe('Mutex', () => {
   });
 
   it('should lock and unlock correctly', async () => {
-    const start = Date.now();
-    let criticalSectionTime = 0;
+    const executionOrder: number[] = [];
 
     // Function to simulate a critical section
-    const criticalSection = async () => {
+    const criticalSection = async (id) => {
       await mutex.lock();
-      const startTime = Date.now();
+      executionOrder.push(id);
       // Simulate some work in the critical section
       await new Promise((resolve) => setTimeout(resolve, 100));
-      criticalSectionTime += Date.now() - startTime;
       mutex.unlock();
     };
 
-    const promise1 = criticalSection();
-    const promise2 = criticalSection();
+    await Promise.all([criticalSection(222), criticalSection(444)]);
 
-    await Promise.all([promise1, promise2]);
-    const totalTime = Date.now() - start;
-
-    // Check if critical section times do not overlap
-    expect(criticalSectionTime).toBeGreaterThanOrEqual(200);
-    expect(totalTime).toBeGreaterThanOrEqual(criticalSectionTime);
+    // Check if execution order is sequential
+    expect(executionOrder).toEqual([222, 444]);
   });
 
   it('should not allow unlocking when not locked', async () => {
@@ -40,20 +33,22 @@ describe('Mutex', () => {
     let firstLockDone = false;
 
     await mutex.lock();
-    const firstLockPromise = new Promise<void>(async (resolve) => {
-      // This lock should execute first
-      await new Promise((resolve) => setTimeout(resolve, 50)); // Simulate work
-      firstLockDone = true;
-      mutex.unlock();
-      resolve();
+    const firstLockPromise = new Promise<void>((resolve) => {
+      setTimeout(() => {
+        firstLockDone = true;
+        mutex.unlock();
+        resolve();
+      }, 1000);
     });
 
-    const secondLockPromise = new Promise<void>(async (resolve) => {
-      await mutex.lock();
-      // Check if the first lock finished before starting the second lock
-      expect(firstLockDone).toBe(true);
-      mutex.unlock();
-      resolve();
+    const secondLockPromise = new Promise<void>((resolve) => {
+      setTimeout(async () => {
+        await mutex.lock();
+        // Check if the first lock finished before starting the second lock
+        expect(firstLockDone).toBe(true);
+        mutex.unlock();
+        resolve();
+      }, 100);
     });
 
     await Promise.all([firstLockPromise, secondLockPromise]);
