18d21cbc8c6bde3910a09e9708cb2173d275000b
Eddort <webisforce@gmail.com>
2024-10-31 12:20:25 +0100

    fix: remove unused vars

--- src/guardian/guardian.constants.ts
+++ src/guardian/guardian.constants.ts
@@ -1,5 +1,3 @@
-import { CronExpression } from '@nestjs/schedule';
-
 export const GUARDIAN_DEPOSIT_RESIGNING_BLOCKS = 10;
 export const GUARDIAN_DEPOSIT_JOB_NAME = 'guardian-deposit-job';
 export const GUARDIAN_DEPOSIT_JOB_DURATION_MS = 5000;
