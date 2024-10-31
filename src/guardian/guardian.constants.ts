8e90483dade8418154e83cc3cc1da653ee75c267
Eddort <webisforce@gmail.com>
2024-10-31 11:37:45 +0100

    feat: replace cronJob by setInterval

--- src/guardian/guardian.constants.ts
+++ src/guardian/guardian.constants.ts
@@ -2,6 +2,6 @@ import { CronExpression } from '@nestjs/schedule';
 
 export const GUARDIAN_DEPOSIT_RESIGNING_BLOCKS = 10;
 export const GUARDIAN_DEPOSIT_JOB_NAME = 'guardian-deposit-job';
-export const GUARDIAN_DEPOSIT_JOB_DURATION = CronExpression.EVERY_5_SECONDS;
+export const GUARDIAN_DEPOSIT_JOB_DURATION_MS = 5000;
 export const MIN_KAPI_VERSION = '2.2.0';
 export const GUARDIAN_PING_BLOCKS_PERIOD = 300;
