a497f1f13921315630d2d3f189ec0715296020d5
Eddort <webisforce@gmail.com>
2025-01-22 14:43:19 +0100

    feat: initial devnet

--- src/messages/messages.constants.ts
+++ src/messages/messages.constants.ts
@@ -1,11 +1,14 @@
 import { CHAINS } from '@lido-sdk/constants';
 
-export const MESSAGE_TOPIC_PREFIX_BY_NETWORK: {
-  [key in CHAINS]?: string;
-} = {
+const LOCAL_DEVNET_CHAIN_ID = 32382;
+const PECTRA_5_DEVNET_CHAIN_ID = 7088110746;
+
+export const MESSAGE_TOPIC_PREFIX_BY_NETWORK = {
   [CHAINS.Mainnet]: 'mainnet',
   [CHAINS.Goerli]: 'goerli',
   [CHAINS.Holesky]: 'holesky',
+  [LOCAL_DEVNET_CHAIN_ID]: 'devnet',
+  [PECTRA_5_DEVNET_CHAIN_ID]: 'devnet-pectra5',
 };
 
 export const getMessageTopicPrefix = (chainId: CHAINS): string => {
