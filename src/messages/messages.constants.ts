0bc472b3f2e9397c8165b06b6019957f44a2f6e6
infloop <infloop.gm@gmail.com>
2025-02-27 16:15:37 +0300

    feat: pectra devnet-7

--- src/messages/messages.constants.ts
+++ src/messages/messages.constants.ts
@@ -3,6 +3,7 @@ import { CHAINS } from '@lido-sdk/constants';
 const LOCAL_DEVNET_CHAIN_ID = 32382;
 const PECTRA_5_DEVNET_CHAIN_ID = 7088110746;
 const PECTRA_6_DEVNET_CHAIN_ID = 7072151312;
+const PECTRA_7_DEVNET_CHAIN_ID = 7032118028;
 
 export const MESSAGE_TOPIC_PREFIX_BY_NETWORK = {
   [CHAINS.Mainnet]: 'mainnet',
@@ -11,6 +12,7 @@ export const MESSAGE_TOPIC_PREFIX_BY_NETWORK = {
   [LOCAL_DEVNET_CHAIN_ID]: 'testnet',
   [PECTRA_5_DEVNET_CHAIN_ID]: 'testnet',
   [PECTRA_6_DEVNET_CHAIN_ID]: 'testnet',
+  [PECTRA_7_DEVNET_CHAIN_ID]: 'testnet',
 };
 
 export const getMessageTopicPrefix = (chainId: CHAINS): string => {
