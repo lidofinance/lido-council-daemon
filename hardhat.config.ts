5d7f1a42226c1d480a02147894f4e52599f7dea2
Eddort <webisforce@gmail.com>
2025-08-18 13:06:29 +0200

    refactor: HH config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,18 +2,21 @@ import { HardhatUserConfig } from 'hardhat/config';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const RPC_URL = process.env.RPC_URL!;
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const secretKey = process.env.WALLET_PRIVATE_KEY!;
+const RPC_URL = process.env.RPC_URL || 'http://localhost:8545';
+const secretKey =
+  process.env.WALLET_PRIVATE_KEY ||
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 const CHAIN_ID = process.env.CHAIN_ID || '17000';
 
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {
-      forking: {
-        url: RPC_URL,
-      },
+      forking:
+        RPC_URL !== 'http://localhost:8545'
+          ? {
+              url: RPC_URL,
+            }
+          : undefined,
       chainId: parseInt(CHAIN_ID, 10),
       accounts: [
         {
