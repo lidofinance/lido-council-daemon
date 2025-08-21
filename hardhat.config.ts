d5e21c9d5c72799df38b05d1ef7c0740256ff22d
Eddort <webisforce@gmail.com>
2025-08-21 14:00:51 +0200

    fix: add logging for RPC_URL in Hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const RPC_URL = process.env.RPC_URL || 'http://127.0.0.1:8545';
 const secretKey = process.env.WALLET_PRIVATE_KEY!;
 
+console.log(`Using RPC URL: ${RPC_URL.split('.')[0]}`);
+
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {
