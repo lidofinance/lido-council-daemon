5b501ad5b8f9cb752a090a42d4f328b5c013cca8
Eddort <webisforce@gmail.com>
2025-08-21 08:58:38 +0200

    fix: add E2E_RPC_URL for e2e tests migration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const RPC_URL = process.env.RPC_URL || 'http://127.0.0.1:8545';
+const RPC_URL = process.env.E2E_RPC_URL || process.env.RPC_URL || 'http://127.0.0.1:8545';
 const secretKey = process.env.WALLET_PRIVATE_KEY!;
 const CHAIN_ID = process.env.CHAIN_ID || '17000';
 
