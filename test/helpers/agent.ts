f035a1bac7fb9dcae96c33796e8157866e41f7b2
Eddort <webisforce@gmail.com>
2025-10-27 14:10:02 +0100

    feat: add Hoodi chain support to AGENT and EVM_SCRIPT_EXECUTOR

--- test/helpers/agent.ts
+++ test/helpers/agent.ts
@@ -3,4 +3,5 @@ import { CHAINS } from 'contracts/repository/locator/locator.constants';
 export const AGENT = {
   [CHAINS.Holesky]: '0xE92329EC7ddB11D25e25b3c21eeBf11f15eB325d',
   [CHAINS.Mainnet]: '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c',
+  [CHAINS.Hoodi]: '0x0534aA41907c9631fae990960bCC72d75fA7cfeD',
 };
