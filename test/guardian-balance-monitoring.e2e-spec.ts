f6a77d31d41d1715f99b2218ec4480db9d983de5
chasingrainbows <chasing.rainbows@lido.fi>
2026-04-01 18:27:47 +0300

    fix: fix asserts

--- test/guardian-balance-monitoring.e2e-spec.ts
+++ test/guardian-balance-monitoring.e2e-spec.ts
@@ -130,6 +130,10 @@ describe('Guardian balance ', () => {
       .map(([message]) => message as { stakingModuleId: number });
   };
 
+  const expectDepositsStillWork = (fromCallIndex = 0) => {
+    expect(getNewDepositMessages(fromCallIndex).length).toBeGreaterThan(0);
+  };
+
   const expectNoDepositsForModule = (moduleId: number, fromCallIndex = 0) => {
     const newDepositMessages = getNewDepositMessages(fromCallIndex);
     expect(
@@ -283,7 +287,7 @@ describe('Guardian balance ', () => {
       // 4 - number of modules
       expect(validateKeys).toHaveBeenCalledTimes(stakingModulesCount);
       expect(sendUnvetMessage).toHaveBeenCalledTimes(0);
-      expectDepositsForModule(1, depositCallsBeforeCycle);
+      expectDepositsStillWork(depositCallsBeforeCycle);
     });
 
     test('Increase staking limit', async () => {
